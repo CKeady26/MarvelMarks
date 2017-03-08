2ae5e08e158372551835f1157a892d90a04e90c0
RaxEmRemy <pwnagepal@gmail.com>
2017-03-08 20:56:25 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include("/var/www/html/projects/bookmarksite/password_protect.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Bookmark Site</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link href="bookmarks.css" type="text/css" rel="stylesheet" />
+		
+	</head>
+	
+	<body>
+		<!-- .preview keeps dimensions of scaled iframe under control -->
+		<div class="preview">
+		  <iframe src="http://www.gamespot.com/" sandbox></iframe>
+		</div>
+
+		<div class="preview">
+		  <iframe src="http://appendto.com" sandbox></iframe>
+		</div>
+		
+	</body>
+	
+	
+</html>
